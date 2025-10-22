9b2795c3550b8bfb21d22d95b69b7cdfae81a517
minalue <min_alu@outlook.com>
2025-10-22 21:08:47 +0300

    footer UI update

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,13 +1,21 @@
-import React from 'react'
+import Image from "next/image";
+import React from "react";
 
 const FooterComponent = () => {
   return (
-    <div className='bottom-0 absolute flex justify-center h-6 text-gray-200 w-full cursor-pointer'>
-        <p className="bg-gray-800 px-4 rounded-t">
-            BIT
-        </p>
+    <div className="bottom-0 absolute flex justify-center h-6 text-gray-200 w-full cursor-pointer">
+      <div className="bg-gray-800 px-2 rounded-t flex items-center gap-1">
+        <Image
+          src={"/bit.jpg"}
+          alt="logo"
+          height={20}
+          width={20}
+          className="rounded-full"
+        />
+        <p> BIT</p>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default FooterComponent
+export default FooterComponent;
